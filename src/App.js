73d320bc1816a7f97bb46cfb467691edc163bc4a
Binod RD <binodatjaper@gmail.com>
2023-03-04 01:53:29 +0545

    add modal

--- src/App.js
+++ src/App.js
@@ -58,8 +58,45 @@ function App() {
                         );
                     })
                 }
-                {formFields.length > 0 ? <button type="submit" className="btn btn-primary">Submit</button> : <></>}
+                {formFields.length > 0 ? <button type="submit" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Submit</button> : <></>}
             </form>
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Form Data</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <table className="table table-bordered">
+                                <tr>
+                                    <th>Name :</th>
+                                    <td>{formDatas.name}</td>
+                                </tr>
+                                <tr>
+                                    <th>Email :</th>
+                                    <td>{formDatas.email}</td>
+                                </tr>
+                                <tr>
+                                    <th>Address :</th>
+                                    <td>{formDatas.address}</td>
+                                </tr>
+                                <tr>
+                                    <th>Age :</th>
+                                    <td>{formDatas.age}</td>
+                                </tr>
+                                <tr>
+                                    <th>Birthday :</th>
+                                    <td>{formDatas.birthday}</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <hr />
 
